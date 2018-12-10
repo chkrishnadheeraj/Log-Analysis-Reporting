6140961dd93742df87f72738374b33089dbace41
Krishna Dheeraj <dheerajkrishna90@gmail.com>
2018-12-10 18:09:07 +0530

    Restyled the code as per pycodestyle guidelines.
    Reference: http://pep8online.com

--- loganalysis.py
+++ loganalysis.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import psycopg2
 
 DBNAME = "news"
@@ -10,12 +11,12 @@ top_articles_query = """select title,count(*) as views
 
 top_authors_query = """select auth.name,count(*) as num_views
                         from authors auth,articles art,article_views
-                        where auth.id = art.author and art.slug=article_views.substring
+                        where auth.id = art.author and
+                        art.slug=article_views.substring
                         group by auth.name
                         order by num_views desc;"""
 
 top_error_query = "select *  from error_view where \"Error Percentage\" > 1"
-
 
 query_1_result = dict()
 query_1_result['title'] = "\nTop 3 popular articles of all time:\n"
@@ -24,28 +25,32 @@ query_2_result = dict()
 query_2_result['title'] = "\nMost popular article authors of all time:\n"
 
 query_3_result = dict()
-query_3_result['title'] = "\nThe days where requests error percentage was greater than 1:\n"
+query_3_result['title'] = """\nThe days where requests error percentage was \
+                          greater than 1:\n"""
 
-#runs the sql queries
+
+# runs the sql queries
 def get_results(query):
-    db = psycopg2.connect(database = DBNAME)
+    db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
     c.execute(query)
     results = c.fetchall()
     db.close()
-    return results 
+    return results
 
-#prints the query results first and second
+
+# prints the query results first and second
 def print_query_results(query_result):
     print(query_result['title'])
     for result in query_result['results']:
         print('\t' + str(result[0]) + '----->' + str(result[1]) + ' views')
 
-#prints the error results
+
+# prints the error results
 def print_error_results(error_result):
     print(error_result['title'])
     for result in error_result['results']:
-        print('\t' + str(result[0]) + '----->' + str(result[1]) + '% errors' )
+        print('\t' + str(result[0]) + '----->' + str(result[1]) + '% errors')
 
 query_1_result['results'] = get_results(top_articles_query)
 query_2_result['results'] = get_results(top_authors_query)
@@ -54,5 +59,3 @@ query_3_result['results'] = get_results(top_error_query)
 print_query_results(query_1_result)
 print_query_results(query_2_result)
 print_error_results(query_3_result)
-
-
